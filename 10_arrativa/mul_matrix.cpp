ce9fc6e071e62e870ac7412017791b3fd10d45c1
Unknown <angel.g.alvar@gmail.com>
2018-02-07 14:34:36 +0100

    10_arrativa matrix multiplicator
    Now we have a working prototype (^_^)

--- 10_arrativa/mul_matrix.cpp
+++ 10_arrativa/mul_matrix.cpp
@@ -2,23 +2,23 @@
 #include<stdlib.h>
 #include<stdio_ext.h>
 
-#define N 3
-#define M N+1
+#define N 10
+#define M N
 
 #define __DEBUG 0
 
 int ask_YN(){
-	int success=0;
-	char ans;
-	do{
-	success=scanf(" %c", &ans);
+    int success=0;
+    char ans;
+    do{
+        success=scanf(" %c", &ans);
 
-	if(success == EOF){
+        if(success == EOF){
             if( __DEBUG   ) fprintf(stderr,"stdin is closed or I/O error\n");
         }else if(success == 0){
             if( __DEBUG   ){
-                 fprintf(stderr,"Some input was available, but did not make sense f    or a double.\n");
-                 fprintf(stderr,"Purgin stdin\n");
+                fprintf(stderr,"Some input was available, but did not make sense f    or a double.\n");
+                fprintf(stderr,"Purgin stdin\n");
             }
             __fpurge(stdin);
         }else if(success == 1){
@@ -26,37 +26,37 @@ int ask_YN(){
             if( __DEBUG   ) fprintf(stderr,"Success getting int\n");
         }
 
-	if ( ans == 'S' || ans == 's' || ans == 'N' || ans == 'n' || ans == 'y' || ans == 'Y' ){
-        success=1;
-   	}else{
-     	printf("\tResponda con 'S', 's', 'N' o 'n'\n");
-     	success=0;
-     	}
-  	}while(!success);
+        if ( ans == 'S' || ans == 's' || ans == 'N' || ans == 'n' || ans == 'y' || ans == 'Y' ){
+            success=1;
+        }else{
+            printf("\tResponda con 'S', 's', 'N' o 'n'\n");
+            success=0;
+        }
+    }while(!success);
 
-  	if (ans=='S' || ans=='s' || ans == 'y' || ans == 'Y' )
-	  	return true;
-	else
-	  	return false;
+    if (ans=='S' || ans=='s' || ans == 'y' || ans == 'Y' )
+        return true;
+    else
+        return false;
 
 
 }
 
-void print_matrix(double (*matrix)[10],int x, int y ){
+void print_matrix(double (*matrix)[M],int x, int y ){
 
-    for(int i=0;i<N;i++){
-        for(int j=0;j<M;j++){
+    for(int i=0;i<y;i++){
+        for(int j=0;j<x;j++){
             printf("%10.4lf", matrix[i][j]);
         }
         printf("\n");
     }
 }
 
-void ask_matrix(double (*matrix)[10],int x, int y ){
+void ask_matrix(double (*matrix)[M],int x, int y ){
     int success=0;
     printf("Introduce una mariz de %i x %i :\n", x, y);
-    for(int i=0;i<x;i++){
-        for(int j=0;j<y;j++){
+    for(int i=0;i<y;i++){
+        for(int j=0;j<x;j++){
             do{
                 if( __DEBUG  ) fprintf(stderr,"Getting matrix [%i][%i]\n",i ,j);
                 printf("Introduce el valor [%i][%i]:\n >",i ,j);
@@ -84,12 +84,12 @@ int ask_int(char const *text, int *num){
         printf("%s", text);
         success = scanf(" %i", num);
 
-	if(success == EOF){
+        if(success == EOF){
             if( __DEBUG   ) fprintf(stderr,"stdin is closed or I/O error\n");
         }else if(success == 0){
             if( __DEBUG   ){
-                 fprintf(stderr,"Some input was available, but did not make sense f    or a double.\n");
-                 fprintf(stderr,"Purgin stdin\n");
+                fprintf(stderr,"Some input was available, but did not make sense f    or a double.\n");
+                fprintf(stderr,"Purgin stdin\n");
             }
             __fpurge(stdin);
         }else if(success == 1){
@@ -99,63 +99,124 @@ int ask_int(char const *text, int *num){
     }while(!success);
 }
 
+int multiply_matrixs(
+        double (*m0)[M], int x0,int y0,
+        double (*m1)[M], int x1,int y1,
+        double (*mr)[M]
+        ){
+    if(y0!=x0){
+        fprintf(stderr,"Error! column of first matrix not equal to row of second.\n");
+        return false;
+    }
+    for(int i=0; i < y0; i++){
+        for(int j=0; j<x1; j++){
+            for(int k=0;k<x0;k++){
+              mr[i][j] += m0[i][k] * m1[k][j];
+            }
+        }
+    }
+    return true;
+
+}
+
 int main(){
 
-    double matrix0[10][10];
-    double matrix1[10][10];
+    double matrix0[N][M];
+    double matrix1[N][M];
+    double matrixR[N][M];
     int matrix0_X,matrix0_Y,matrix1_X,matrix1_Y;
 
-    int success=0, sure=0;
+    int success=0;
 
-  do{
+    // Ask matrixs dimmensions
     do{
-        printf("Enter rows and columns for first matrix:\n");
+        // First matrix
+        do{
+            printf("Enter rows and columns for first matrix:\n");
 
-        ask_int("  First matrix rows:", &matrix0_Y);
-        //printf("First matrix rows: %i\n", matrix0_Y);
-        ask_int("  First matrix columns:", &matrix0_X);
-        //printf("First matrix columns: %i\n", matrix0_X);
+            ask_int("  First matrix rows:", &matrix0_Y);
+            //printf("First matrix rows: %i\n", matrix0_Y);
+            ask_int("  First matrix columns:", &matrix0_X);
+            //printf("First matrix columns: %i\n", matrix0_X);
 
-        if(matrix0_Y >0 && matrix0_X >0)
-        	success=1;
-        else{
-        printf("dimension must be greater than 0");
-        	continue;
+            if(matrix0_Y >0 && matrix0_X >0)
+                success=1;
+            else{
+                printf("dimension must be greater than 0\n");
+                continue;
+            }
+
+            printf("Are the dimmensions correct? [y/n] ");
+            if (!ask_YN())
+                success=0;
+
+        }while(!success);
+        // Second matrix
+        do{
+            printf("Enter rows and columns for second matrix:\n");
+
+            ask_int("  Second matrix rows:", &matrix1_Y);
+            //printf("Second matrix rows: %i\n", matrix1_Y);
+            ask_int("  Second matrix columns:", &matrix1_X);
+            //printf("Second matrix columns: %i\n", matrix1_X);
+
+            if(matrix1_Y >1 && matrix1_X >1)
+                success=1;
+
+            printf("Are the dimmensions correct? [y/n] ");
+            if (!ask_YN())
+                success=0;
+
+        }while(!success);
+
+        if(matrix0_X != matrix1_Y){
+            printf("Error! column of first matrix not equal to row of second.\n");
+            success=0;
         }
 
-        printf("Are the dimmensions correct? [y/n] ");
-        if (!ask_YN())
-        	success=0;
-
     }while(!success);
 
+    // Ask matrix
+
+      // first matrix
     do{
-        printf("Enter rows and columns for second matrix:\n");
+        success=1;
+        printf("\n Introduzca la primera matriz\n");
+        ask_matrix( matrix0, matrix0_X, matrix0_Y );
 
-        ask_int("  Second matrix rows:", &matrix1_Y);
-        //printf("Second matrix rows: %i\n", matrix1_Y);
-        ask_int("  Second matrix columns:", &matrix1_X);
-        //printf("Second matrix columns: %i\n", matrix1_X);
-
-        if(matrix1_Y >1 && matrix1_X >1)
-        	success=1;
-
-        printf("Are the dimmensions correct? [y/n] ");
+        printf("\n Primera matriz:\n");
+        print_matrix(matrix0,matrix0_X, matrix0_Y );
+        printf("\nAre the dimmensions correct? [y/n] ");
         if (!ask_YN())
-        	success=0;
+            success=0;
+    }while(!success);
+      // second matrix
+    do{
+        success=1;
+        printf("\n Introduzca la segunda matriz\n");
+        ask_matrix( matrix1, matrix1_X, matrix1_Y );
 
+        printf("\n Segunda matriz:\n");
+        print_matrix(matrix1,matrix1_X, matrix1_Y );
+        printf("\nAre the dimmensions correct? [y/n] ");
+        if (!ask_YN())
+            success=0;
     }while(!success);
 
-    if(matrix0_X != matrix1_Y){
-	printf("Error! column of first matrix not equal to row of second.\n");
-        	success=0;
+    // multiply
+    success=multiply_matrixs(
+            matrix0, matrix0_X, matrix0_Y,
+            matrix1, matrix1_X, matrix1_Y,
+            matrixR);
+    if(!success){
+        fprintf(stderr, "there were errors in the multiplication of the matrixs\n"
+                        "Exiting now\n!");
+            return 1;
     }
 
-  }while(!success);
+    printf("\nResulting matrix:\n");
+    print_matrix(matrixR, matrix1_X, matrix0_Y);
 
-
-
-    //ask_matrix( matrix0  );
 
     return EXIT_SUCCESS;
 }
