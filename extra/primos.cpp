bad70187aec70459334ab13d3df339ac67cd2459
Unknown <angel.g.alvar@gmail.com>
2018-02-12 14:21:54 +0100

    Prime number finder improved
    now we use malloc

--- extra/primos.cpp
+++ extra/primos.cpp
@@ -1,18 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdio_ext.h>
 
 #define M 15000
 
 #define verbose 0
 
+int ask_int(char const *text){
+    int num;
+    int success;
+    do{
+        printf("%s", text);
+        success = scanf(" %i", &num);
+
+        if(success == EOF){
+            if( verbose ) fprintf(stderr,"stdin is closed or I/O error\n");
+        }else if(success == 0){
+            if( verbose ){
+                fprintf(stderr,"Some input was available, but did not make sense f    or a double.\n");
+                fprintf(stderr,"Purgin stdin\n");
+            }
+            __fpurge(stdin);
+        }else if(success == 1){
+            //success
+            if( verbose ) fprintf(stderr,"Success getting int\n");
+        }
+    }while(!success);
+    return num;
+}
+
 int main(){
 
-    int  arr[M];
-    int rem=M, remo=M;
+    int msize=20;
+    msize=ask_int("Introduce el numero maximo: ")+1;
+    int *arr = (int*) malloc( msize*sizeof(int) );
+
+    //int  arr[M];
+    //int rem=M, remo=M;
+    int rem=msize, remo=msize;
     //int *p_next = &arr[3];
     int next;
 
-    for(int i=0; i<M; i++)
+    for(int i=0; i<msize; i++)
         arr[i]=i;
 
     for(int i=2; i<rem; i++){
@@ -33,10 +62,14 @@ int main(){
             }
         }
         rem=remo;
+        arr = (int*) realloc (arr, rem * sizeof(int));
+        
+
     }
 
     for(int i=0; i<rem; i++)
         printf("%i, ", arr[i]);
     printf("\n");
+    free(arr);
     return EXIT_SUCCESS;
 }
