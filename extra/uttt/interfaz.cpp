0330ec1769f96b3a5424286a749858dc38a2511f
Angel García <angel.g.alvar@gmail.com>
2018-03-09 15:08:24 +0100

    Ultimate Tic Tac Toe init
    UTTT proyect

---
+++ extra/uttt/interfaz.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Some garbage, ejem frames
+// I put then here yo have then near
+
+// ─│┌┐└┘├┤┬┴┼
+// ━┃┏┓┗┛┣┫┳┻╋
+// ═║╔╗╚╝╠╣╦╩╬
+// ╭╮╯╰
+// ┍┎┑┒┕┖┙┚┝┞┟┠┡┢┥┦┧┨┩┪┭┮┯┰┱┲┵┶┷┸┹┺┽┾┿╀╁╂╃╄╅╆╇╈╉╊
+// ╒╓╕╖╘╙╛╜╞╟╠╡╢╤╥╧╨╪╫
+
+int main(){
+
+    
+    return EXIT_SUCCESS;
+}
