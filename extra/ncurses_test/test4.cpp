54f97cf33fdf1d9e5d916d4c7adbf672ca3f4930
Unknown <angel.g.alvar@gmail.com>
2018-04-20 14:34:52 +0200

    big update

---
+++ extra/ncurses_test/test4.cpp
@@ -0,0 +1,51 @@
+#define _XOPEN_SOURCE_EXTENDED
+#include <ncursesw/ncurses.h>
+#include <locale.h>
+
+int main()
+{
+    wint_t ch;
+    //cchar_t ch;
+
+    setlocale(LC_ALL, "en_US");
+    initscr();                      /* Inicia el modo curses            */
+    raw();                          /* Desactiva buffer de linea        */
+    keypad(stdscr, TRUE);           /* Obtener F1, F2 etc..d            */
+    noecho();                       /* Sin echo() al hacer getch        */
+
+    do{
+    raw();
+    printw( "pulse cualquier caracter para verlo remarcado\n"
+            "o pulse F10 para salir");
+    get_wch(&ch);
+    /*ch = getch();                   /* Si no ha llamado a raw() tenemos
+                                     * que pulsar enter antes que que
+                                     * que llegue al programa           */
+    if(ch == KEY_F(1))              /* sin habilitar keypad esto no     */
+        printw("F1 Key pressed");     /* llegara a nosotros               */
+                                      /* Sin noecho() algunos feos
+                                       * caracteres de escape podrian ser
+                                       * impresos en pantalla             */
+    else
+    {
+        if(ch == KEY_F(10)){
+            printw("Pulse cualquier tecla para salir ");
+        }
+        else{
+            printw("La tecla pulsada es ");
+            attron(A_BOLD); // atribute on BOLD
+            printw("%c", ch);
+            //addwstr(&ch);
+            attroff(A_BOLD); // disable A_BOLD atttribute
+        }
+    }
+
+    wrefresh(stdscr);                      /* Imprime stdscr en la pantalla real      */
+    halfdelay(20);
+    getch();
+    clear();
+    }while(ch!=KEY_F(10));
+    endwin();
+    return 0;
+
+}
